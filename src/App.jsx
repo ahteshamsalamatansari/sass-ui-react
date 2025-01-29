8054196ff95545852a0ce6dd49008787b18840d4
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-29 22:07:38 +0530

    Router Dom Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
-
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from "./components/Button"
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden '>
+      <Button className='mt-10' href="#login">
+        Something 
+      </Button>
+    </div>
+    <ButtonGradient/>
+    </>
+  );
 }
 
 export default App
