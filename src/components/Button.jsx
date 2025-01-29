40200ae19841d7d0081888b6c50970e6a544db25
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-30 00:25:30 +0530

    Button component is ready now

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative  inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-1  ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
@@ -10,7 +10,14 @@ const Button = ({ className, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+  const renderLink = () => (
+    <a href={href} target="_blank" className={classes} rel="noopener noreferrer">
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  )
+  
+  return href ? renderLink() : renderButton()
 
 };
 
