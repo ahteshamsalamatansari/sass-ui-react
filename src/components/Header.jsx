88c4a7c24af77f22b492d3e869b228ce5003e06c
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-30 01:39:54 +0530

    Particles section importing......

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import ButtonGradient from "../assets/svg/ButtonGradient";
 const Header = () => {
   const pathName = useLocation();
   return (
-    <div className="fixed top-0 z-50  bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-full z-50  bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8 ">
           <img src={brainwave} alt="Brainwave" width={190} height={40} />
