c969db7e307c869efb48b9ba2e28b64491d26572
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-01-30 22:31:29 +0530

    Header almost ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,30 @@ import { navigation } from "../constants";
 import { useLocation } from "react-router-dom";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/Menusvg";
-import { HamburgerMenu} from "./design/Header"
+import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
 const Header = () => {
   const pathName = useLocation();
-  const [openNavigation, setopenNavigation] = useState(false)
+  const [openNavigation, setopenNavigation] = useState(false);
+  const toggleNavigation = () => setopenNavigation(!openNavigation);
+  const handleClick = () => {
+    if (!openNavigation) return;
+  }
   return (
-    <div className={`fixed top-0 left-0 w-full z-50  bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'}`}>
+    <div
+      className={`fixed top-0 left-0 w-full z-50  bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8 ">
           <img src={brainwave} alt="Brainwave" width={190} height={40} />
         </a>
-        <nav className={`${openNavigation?'flex':'hidden'}  fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:flex lg:static lg:mx-auto lg:bg-transparent `}>
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          }  fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:flex lg:static lg:mx-auto lg:bg-transparent `}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -25,15 +37,15 @@ const Header = () => {
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathName.hash
                     ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50 "
+                    : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                onClick={handleClick}
               >
                 {item.title}
               </a>
             ))}
-            <HamburgerMenu/>
           </div>
-
+          <HamburgerMenu />
         </nav>
         <a
           href="#signup"
@@ -41,11 +53,15 @@ const Header = () => {
         >
           New Account
         </a>
-        <Button className="hidden lg:flex" href="#login">
+        <Button className="!hidden lg:!flex" href="#login">
           Sign In
         </Button>
-        <Button className="ml-auto lg:hidden" px="px-3">
-            <MenuSvg openNavigation={openNavigation}/>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
