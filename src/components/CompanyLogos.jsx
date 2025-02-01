9d4ffac8fe8c10759f12f9f5fef7c6188999e96b
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-01 16:18:42 +0530

    Going towards logos and

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const CompanyLogos = () => {
+const CompanyLogos = ({className}) => {
   return (
-    <div>CompanyLogos</div>
+    <div className={className}>
+      <h5 className='tagline mb-6 text-center text-n-1/50'>Helping people to create beautiful content at</h5>
+      
+    </div>
   )
 }
 
