6caecf011db9340e3dae588779bd7a84a1d8a0f2
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-01 13:09:21 +0530

    35% of hero section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 const App = () => {
   return (
     <>
-    <div className='pt-[4.75rem] lg:pt-[5.25rem]'>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
      <Header/> 
      <Hero/>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import Section from "./Section";
-import { curve, robot } from "../assets";
+import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 const Hero = () => {
   return (
@@ -10,9 +10,9 @@ const Hero = () => {
       id="hero"
     >
       <div className="container relative ">
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possiblities of AI Chatting with BrainwaveCurve
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -32,16 +32,30 @@ const Hero = () => {
             Get Started
           </Button>
         </div>
-        <div className="realative max-w-[23rem] mx-auto md:mx-w-5xl xl:mb-24">
-           <div className="realative z-1 p-0.5 rounded-2xl bg-conic-gradient">
-              <div className="relative bg-n-8 rounded-[1rem]">
-                 <div className="h-[1.4rem] bg-n-8 rounded-t-[0.9rem]">
-                    <div>
-                      <img src={robot} className="w-full" width={1440} height={1800} alt="" />
-                    </div>
-                  </div> 
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  className="w-full"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
               </div>
-           </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="Robot"
+            />
+          </div>
         </div>
       </div>
     </Section>
