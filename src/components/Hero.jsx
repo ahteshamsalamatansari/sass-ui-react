07fa320fd11c336e20811183a32a613714815e40
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-01 15:21:38 +0530

    Parallax effect applied successfully

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
 import Section from "./Section";
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 const Hero = () => {
+  const parallaxRef = useRef(null);
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -9,7 +14,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative ">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
@@ -44,6 +49,15 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} alt={icon} width={24} height={25} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
           </div>
