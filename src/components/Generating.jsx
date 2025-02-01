a6c3f4e19d9bfad523c9fc174aa5a0e716785086
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-01 15:34:04 +0530

    Generating component created

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,7 +1,16 @@
-const Generating = () => {
-  return (
-    <div>Generating</div>
-  )
-}
+import { loading } from "../assets";
 
-export default Generating
+const Generating = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
+        className || ""
+      } text-base`}
+    >
+      <img src={loading} alt="loading" className="w-5 h-5 mr-4" />
+      AI is Generating
+    </div>
+  );
+};
+
+export default Generating;
