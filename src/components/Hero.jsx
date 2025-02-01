d77888f4ba41c55b4efb2f1f32eed31459cba632
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-01 15:28:47 +0530

    Moving towards generating

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -60,6 +60,8 @@ const Hero = () => {
                 </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -70,8 +72,10 @@ const Hero = () => {
               alt="Robot"
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
+      <BottomLine />
     </Section>
   );
 };
