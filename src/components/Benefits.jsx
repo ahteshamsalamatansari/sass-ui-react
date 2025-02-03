b06cf9a50ddb8a1786dd2d4997932c79b239406d
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-04 01:16:25 +0530

    Card in progress

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,9 +8,14 @@ const Benefits = () => {
             <Heading className="md:max-w-md lg:max-w-2xl" title= "Chat Smarter, Not Harder with Brainwave"/>
             <div className='flex flex-wrap gap-10 mb-10'>
               {benefits.map((item)=>(
-                  <div key={item.id}>
-                    <div>
-                      <h5>{item.title}</h5>
+                  <div className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]' 
+                  style={{
+                    backgroundImage: `url(${item.backgroundUrl})`,
+                  }}
+                  key={item.id}>
+                    <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
+                      <h5 className='h2 mb-5'>{item.title}</h5>
+                      <p>{item.text}</p>
                     </div>
                   </div>
               ))}
