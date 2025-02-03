981be50bc15e681abbf73cb78d97a8579ef64a7f
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-04 01:10:52 +0530

    Mapping completed, creation started.

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -7,7 +7,13 @@ const Benefits = () => {
         <div className='container relative z-2'>
             <Heading className="md:max-w-md lg:max-w-2xl" title= "Chat Smarter, Not Harder with Brainwave"/>
             <div className='flex flex-wrap gap-10 mb-10'>
-              {benefits}
+              {benefits.map((item)=>(
+                  <div key={item.id}>
+                    <div>
+                      <h5>{item.title}</h5>
+                    </div>
+                  </div>
+              ))}
             </div>
         </div>
    </Section> 
