eb93183e1571511d10eb9a40f19c7dda3b828a6e
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-05 01:15:15 +0530

    Collaboration section is completed now😍

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -2,11 +2,12 @@ import Section from "./Section";
 import { collabApps, collabContent, collabText } from "../constants";
 import { brainwaveSymbol, check } from "../assets";
 import Button from "./Button";
+import {LeftCurve, RightCurve} from "./design/Collaboration"; 
 const Collaboration = () => {
   return (
     <Section crosses>
       <div className="container lg:flex">
-        <div className="max-w-[25rem]">
+        <div className="max-w-[25rem] md:ml-6">
           <h2 className="h2 mb-4 md:mb-8">
             AI Chat App for seamless collaboration
           </h2>
@@ -25,11 +26,11 @@ const Collaboration = () => {
           </ul>
           <Button>Try it now</Button>
         </div>
-        <div className="lg:ml-auto xl:w-[32rem]">
-          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto mt-4">
+        <div className="lg:ml-auto xl:w-[32rem] md:mr-6 ">
+          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto mt-4">
             {collabText}
           </p>
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+          <div className="mt-4 relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
@@ -43,14 +44,31 @@ const Collaboration = () => {
               </div>
             </div>
             <ul>
-                {collabApps.map((app, index)=>(
-                    <li key={app.id} className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${index*45}`}>
-                        <div className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${index * 45}`}>
-                            <img className="m-auto" width={app.width} height={app.height} src={app.icon} alt={app.title} />
-                        </div>
-                    </li>
-                ))}
+              {collabApps.map((app, index) => (
+                <li
+                  key={app.id}
+                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
+                    index * 45
+                  }`}
+                >
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                      index * 45
+                    }`}
+                  >
+                    <img
+                      className="m-auto"
+                      width={app.width}
+                      height={app.height}
+                      src={app.icon}
+                      alt={app.title}
+                    />
+                  </div>
+                </li>
+              ))}
             </ul>
+            <LeftCurve/>
+            <RightCurve/>
           </div>
         </div>
       </div>
