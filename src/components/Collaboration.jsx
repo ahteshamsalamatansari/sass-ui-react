4d945d5684be5eeb441e64ff4739913f5b9c7a69
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-05 00:56:41 +0530

    Brainwave symbol implemented

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,6 +1,6 @@
 import Section from "./Section";
 import { collabContent, collabText } from "../constants";
-import { check } from "../assets";
+import { brainwaveSymbol, check } from "../assets";
 import Button from './Button';
 const Collaboration = () => {
   return (
@@ -31,7 +31,11 @@ const Collaboration = () => {
                 </p>
                 <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
                     <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
-                        
+                        <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                            <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                                <img src={brainwaveSymbol} width={48} height={48} alt="Brainwave" />
+                            </div>
+                        </div>
                     </div>
                 </div>
         </div>
