d30deff7d65d46cdb33930483d4f0fc8bed12f65
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-05 21:53:49 +0530

    Working on bento-box

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,4 @@
+import { service1 } from '../assets';
 import Heading from './Heading';
 import Section from './Section';
 const Services = () => {
@@ -5,6 +6,13 @@ const Services = () => {
     <Section id="How-to-use">
         <div className='container'>
             <Heading title="Generative AI is Made for Creators" text="Brainwave unlocks the potential of AI-powered applications"/>
+        </div>
+        <div className='relative'>
+            <div className='relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]'>
+                <div>
+                    <img src={service1} alt="" />
+                </div>
+            </div>
         </div>
     </Section>
 
