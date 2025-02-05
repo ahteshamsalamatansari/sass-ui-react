aea1443acef79afe1004c445391026074482facb
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-05 21:47:56 +0530

    Working on bentobox

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,11 @@
+import Heading from './Heading';
 import Section from './Section';
 const Services = () => {
   return (
     <Section id="How-to-use">
-        
+        <div className='container'>
+            <Heading title="Generative AI is Made for Creators" text="Brainwave unlocks the potential of AI-powered applications"/>
+        </div>
     </Section>
 
   )
