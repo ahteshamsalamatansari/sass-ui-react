3a49d1fe399c7013eab682557d7c3ab0c87da13b
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-06 22:04:32 +0530

    Working on bentobox

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import { check, service1 } from "../assets";
 import Heading from "./Heading";
 import Section from "./Section";
-import {brainwaveServices, brainwaveServicesIcons} from '../constants';
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 const Services = () => {
   return (
     <Section id="How-to-use">
@@ -10,29 +10,34 @@ const Services = () => {
           title="Generative AI is Made for Creators"
           text="Brainwave unlocks the potential of AI-powered applications"
         />
-      </div>
-      <div className="relative">
-        <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-          <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
-            <img
-              src={service1}
-              className="w-full h-full object-cover md:object-right"
-              width={800}
-              height={730}
-              alt="Smartest AI"
-            />
-          </div>
-          <div className="relative z-1 max-w-[17rem] h-auto">
-            <h4 className="h-4 mb-4">Smartest AI</h4>
-            <p className="body-2 mb-[13rem] text-n-3">Brainwave unlocks the potential of AI-powered applications</p>
-            <ul className="body-2 ">
-              {brainwaveServices.map((item,index)=>(
-                <li key={index} className="flex items-start py-4 border-t border-n-6">
-                  <img width={24} height={24} src={check} alt="" />
-                  <p className="ml-4">{item}</p>
-                </li>
-              ))}
-            </ul>
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+              <img
+                src={service1}
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                height={730}
+                alt="Smartest AI"
+              />
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h-4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[13rem] text-n-3">
+                Brainwave unlocks the potential of AI-powered applications
+              </p>
+              <ul className="body-2 ">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img width={24} height={24} src={check} alt="" />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
