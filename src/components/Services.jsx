3a8f2947bd83ca51d6ad67f1c928eb963f0775b5
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-10 21:35:17 +0530

    Working on bentobox

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
           title="Generative AI is Made for Creators"
           text="Brainwave unlocks the potential of AI-powered applications"
         />
-        <div className="relative">
+        <div className="relative md:m-10">
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
@@ -52,7 +52,7 @@ const Services = () => {
                   height={750}
                 />
               </div>
-              <div className="relative z-1 max-w-[17rem] ml-auto">
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0  to-n-8/90 lg:p-15 ">
                 <h4 className="h-4 mb-4">Smartest AI</h4>
                 <p className="body-2 mb-[13rem] text-n-3">
                   Brainwave unlocks the potential of AI-powered applications
