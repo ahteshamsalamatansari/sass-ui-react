34a4f011fce3b5819130e25f4112ac90c1820bf2
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 20:01:35 +0530

    Working on pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,11 +1,12 @@
 import Section from './Section';
+import {smallSphere} from '../assets'
 
 const Pricing = () => {
   return (
     <Section className="overflow-hidden" id="pricing">
         <div className='container relative z-2'>
             <div className='hidden relative justify-center mb-[6.5rem] lg:flex'>
-                
+                <img src={smallSphere} alt="" />
             </div>
 
         </div>
