2fbd3a6ac88f99604c1b152323653e37e368fa16
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 21:15:38 +0530

    Working on pricing section

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,16 +1,18 @@
-import {pricing} from "../constants"
-
+import { pricing } from "../constants";
 
 const Pricinglist = () => {
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
-        {pricing.map((item)=>(
-            <div id={item.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8">
-                <h4>{item.title}</h4>
-            </div>
-        ))} 
+      {pricing.map((item) => (
+        <div
+          key={item.id}
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
+        >
+          <h4 className="h4 mb-4">{item.title}</h4>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Pricinglist
+export default Pricinglist;
