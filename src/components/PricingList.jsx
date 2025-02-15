ad0d90001b6bc29a466772a99157b5efbfe4721e
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 21:30:08 +0530

    Styling the pricing list component

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -11,7 +11,7 @@ const Pricinglist = () => {
           className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
-          <p>{item.description}</p>
+          <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">{item.description}</p>
           <div>
             {item.price && (
               <>
