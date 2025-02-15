f7192da6968b5b68bd14a05bd5bd1522bcbd28c8
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 20:59:43 +0530

    Tagline component almost ready

--- src/components/Tagline.jsx
+++ src/components/Tagline.jsx
@@ -4,6 +4,7 @@ const Tagline = ({className, children}) => {
   return (
     <div className={`tagline flex items-center ${className || ''}`}>
         {brackets ('left')}
+        <div className='mx-3 text-n-3'>{children}</div>
         {brackets ('right')}
     </div>
   )
