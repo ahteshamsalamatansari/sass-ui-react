8f0c31bcc0c86b8f6b47ec9e371a55ce014554be
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 21:20:16 +0530

    Pricing list structure is almost ready

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,4 +1,5 @@
 import { pricing } from "../constants";
+import Button from './Button';
 
 const Pricinglist = () => {
   return (
@@ -9,6 +10,18 @@ const Pricinglist = () => {
           className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
         >
           <h4 className="h4 mb-4">{item.title}</h4>
+          <p>{item.description}</p>
+          <div>
+            {item.price && (
+              <>
+                <div> $ </div>
+                <div>{item.price}</div>
+              </>
+            )}
+          </div>
+          <Button>
+
+          </Button>
         </div>
       ))}
     </div>
