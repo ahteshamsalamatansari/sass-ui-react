e0698736557ac410579736687ddc7e3b106dc82d
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-15 21:14:45 +0530

    Working on PricingList Component

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,8 +1,15 @@
+import {pricing} from "../constants"
 
 
 const Pricinglist = () => {
   return (
-    <div>Pricinglist</div>
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+        {pricing.map((item)=>(
+            <div id={item.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8">
+                <h4>{item.title}</h4>
+            </div>
+        ))} 
+    </div>
   )
 }
 
