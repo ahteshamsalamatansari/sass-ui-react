7d7b11b9062fbee39b0fff11436667b089c71837
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 16:04:32 +0530

    Working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import Section from './Section';
+import Section from "./Section";
 
 const Footer = () => {
   return (
-    <Section crosses className='!px-0 !py-10 '>
-        <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col'>
-             
-        </div>
+    <Section crosses className="!px-0 !py-10 ">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p>{new Date().getFullYear()} All Rights Reserved</p>
+      </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
