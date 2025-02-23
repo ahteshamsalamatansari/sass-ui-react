6efba739f083f72c3160f618ef33dd0514406f39
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 15:16:34 +0530

    Working on roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,31 +1,42 @@
 import Section from "./Section";
-import Heading from "./Heading"
+import Heading from "./Heading";
 
 import { roadmap } from "../constants";
 import { grid } from "../assets";
+import Tagline from "./Tagline";
 const Roadmap = () => {
   return (
     <Section className="overflow-hidden" id="roadmap">
-        <div className="container md:pb-10">
-            <Heading tag="Ready to get started" title="What we're working on"/>
-            <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
-                {roadmap.map((item)=>{
-                    const status = item.status==='done'?'Done':"In Progress";
-                    return(
-                      <div key={item.id}>
-                        <div>
-                          <div>
-                            <img src={grid} alt="" />
-                          </div>
-                        </div>
-                      </div>
-                    )
-                })}
-
-            </div>
-        </div>    
+      <div className="container md:pb-10">
+        <Heading tag="Ready to get started" title="What we're working on" />
+        <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {roadmap.map((item) => {
+            const status = item.status === "done" ? "Done" : "In Progress";
+            return (
+              <div key={item.id}>
+                <div>
+                  <div>
+                    <img
+                      src={grid}
+                      alt="grid"
+                      className="w-full"
+                      width={550}
+                      height={550}
+                    />
+                  </div>
+                  <div className="relative z-1">
+                    <div className="flex items-center justify-between">
+                      <Tagline>{item.date}</Tagline>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Roadmap
+export default Roadmap;
