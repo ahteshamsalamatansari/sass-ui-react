f8351ea8a5c42a0486753807b454fe0dd4d6e6ce
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 12:02:29 +0530

    Working on the roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -5,7 +5,8 @@ const Roadmap = () => {
     <Section className="overflow-hidden" id="roadmap">
         <div className="container md:pb-10">
             <Heading tag="Ready to get started" title="What we're working on"/>
-            <div>
+            <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+                {roadmap}
 
             </div>
         </div>    
