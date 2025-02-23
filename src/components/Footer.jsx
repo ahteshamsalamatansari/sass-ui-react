9c837f16db7ce9aacc4a6ba8e8ad326a7ed053ba
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 16:12:17 +0530

    Working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,20 @@
 import Section from "./Section";
-import {socials} from "../constants";
+import { socials } from "../constants";
 
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10 ">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
-        <p className="caption text-n-4 lg:block"> © {new Date().getFullYear()}. All Rights Reserved</p>
+        <p className="caption text-n-4 lg:block">
+          {" "}
+          © {new Date().getFullYear()}. All Rights Reserved
+        </p>
         <ul className="flex gap-5 flex-wrap">
-            {socials}
+          {socials.map((item) => (
+            <a href={item.url} key={item.id} target="_blank">
+              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
+            </a>
+          ))}
         </ul>
       </div>
     </Section>
