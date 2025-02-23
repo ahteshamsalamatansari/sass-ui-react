25961ea12136dd952f4a2b0f27e33b82646637f2
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 11:58:33 +0530

    Working on roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,9 +1,12 @@
 import Section from "./Section";
-
+import Heading from "./Heading";
 const Roadmap = () => {
   return (
     <Section className="overflow-hidden" id="roadmap">
-            
+        <div className="container md:pb-10">
+            <Heading/>
+        
+        </div>    
     </Section>
   )
 }
