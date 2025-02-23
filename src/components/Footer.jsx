2d14ebb9ba27aad851f493262a267a848a7f938f
Ahtesham Salamat Ansari <rdxahteshamsalamat@outlook.com>
2025-02-23 16:10:13 +0530

    Social icons imported in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import Section from "./Section";
+import {socials} from "../constants";
 
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10 ">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block"> © {new Date().getFullYear()}. All Rights Reserved</p>
-        
+        <ul className="flex gap-5 flex-wrap">
+            {socials}
+        </ul>
       </div>
     </Section>
   );
